f1e6a9dab3e8be8988a17a5311f956e5d616c836
Sergio Gil <sergio@wonderbrands.co>
2026-04-21 11:47:39 -0600

    [UPD] manifest

--- __init__.py
+++ __init__.py
@@ -1,1 +1,7 @@
 from . import models
+
+
+def _recompute_product_structure(env):
+    """Recalcula product_structure para todos los productos tras actualizar."""
+    products = env['product.product'].search([])
+    products._compute_product_structure()
